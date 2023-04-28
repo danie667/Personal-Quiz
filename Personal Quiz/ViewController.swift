8f2ec0ab0a5512eb43054956522090316828081a
danie667 <bombuuk@gmail.com>
2023-04-28 19:14:29 +0600

    Initial Commit

---
+++ Personal Quiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Personal Quiz
+//
+//  Created by Daniyal Abuov on 28.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
